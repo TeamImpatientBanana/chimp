d72b12007e7cdf98c7546088de8d078b2706478e
Jarvl <ajarvis7771@gmail.com>
2014-11-14 23:51:03 -0500

    testing again

--- webhook.php
+++ webhook.php
@@ -1,3 +1,14 @@
 <?php
+echo '<pre>';
 
-system('git pull chimp master');
+// Outputs all the result of shellcommand "ls", and returns
+// the last output line into $last_line. Stores the return value
+// of the shell command in $retval.
+$last_line = system('ls', $retval);
+
+// Printing additional info
+echo '
+</pre>
+<hr />Last line of the output: ' . $last_line . '
+<hr />Return value: ' . $retval;
+?>
