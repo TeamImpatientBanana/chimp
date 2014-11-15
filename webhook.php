b9242e7d3b6ca1a5fdca81932c0a7b479710c727
Jarvl <ajarvis7771@gmail.com>
2014-11-14 23:02:00 -0500

    I think the webhook works now

--- webhook.php
+++ webhook.php
@@ -1,8 +1,11 @@
 <?php
+ 
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+ 
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /var/www/impatientbanana.com/public_html/chimp && git pull' );
+  http_response_code(200);
+}
 
-$request_body = file_get_contents('php://input');
-$data = json_decode($request_body);
-
-echo $data
-
+ 
 ?>
