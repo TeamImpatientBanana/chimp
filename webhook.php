73b067ad4e971cab55afc17b13b6642d3711b2d8
Jarvl <ajarvis7771@gmail.com>
2014-11-14 23:05:18 -0500

    trying response message

--- webhook.php
+++ webhook.php
@@ -4,7 +4,7 @@
  
 if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/impatientbanana.com/public_html/chimp && git pull' );
-  http_response_code(200);
+  header("Repository updated!", true, 200);
 }
 
  
