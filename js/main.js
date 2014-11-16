cef3fc9320ea3468ec0875189e67eab8c1af44d2
Jarvl <ajarvis7771@gmail.com>
2014-11-15 23:15:36 -0500

    Links work properly

--- js/main.js
+++ js/main.js
@@ -199,19 +199,19 @@ function clickGenerateButton() {
 
     // Generate buzz word and insert text
     var generatedBuzzWord = generateBuzzWord();
-    $("#ideaBuzzWord").text(generatedBuzzWord.name);
+    $("#ideaBuzzWord").html("<a href='" + generatedBuzzWord.link + "' target='_blank'>" + generatedBuzzWord.name + "</a>");
 
     // Generate noun and insert text
     var generatedNoun = generateNoun();
-    $("#ideaNoun").text(generatedNoun.name);
+    $("#ideaNoun").html("<a href='" + generatedNoun.link + "' target='_blank'>" + generatedNoun.name + "</a>");
 
     // Generate tech words then insert both into text
     var generatedTechs = generateTechs();
-    $("#ideaTech1").text(generatedTechs.tech1.name);
-    $("#ideaTech2").text(generatedTechs.tech2.name);
+    $("#ideaTech1").html("<a href='" + generatedTechs.tech1.link + "' target='_blank'>" + generatedTechs.tech1.name + "</a>");
+    $("#ideaTech2").html("<a href='" + generatedTechs.tech2.link + "' target='_blank'>" + generatedTechs.tech2.name + "</a>");
 
     //Make the Twitter button have your idea in the text
-    $("#twitter-share-button").attr("href", "https://twitter.com/intent/tweet?original_referer=&text=This%20%23hackathon%20I%27m%20going%20to%20make%20"+generatedName+"%2C%20"+generatedBuzzWord.name+"%20"+generatedNoun.name+"%20using%20"+generatedTechs.tech1.name+"%20and%20"+generatedTechs.tech2.name+"%2C%20thanks%20%40impatientbanana%20for%20the%20idea!");
+    $("#twitter-share-button").attr("href", "https://twitter.com/intent/tweet?original_referer=&text=This%20%23hackathon%20I%27m%20going%20to%20make%20"+generatedName+"%2C%20"+generatedBuzzWord.name+"%20"+generatedNoun.name+"%20using%20"+generatedTechs.tech1.name+"%20and%20"+generatedTechs.tech2.name+"%2C%20thanks%20%40impatientbanana!");
 }
 
 // This is the function that animates the scrolling down when the chevron is clicked
