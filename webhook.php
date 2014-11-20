49c5401d87237ae1b9fa4ddccc34e0cbbd33767c
Joe Duncko <dotmaners@gmail.com>
2014-11-20 18:14:20 -0500

    Deleted broken webhook
    Andrew is a scrub
    He could not get the web hook
    To hook any web

--- webhook.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$gitpull = shell_exec('cd /var/www/impatientbanana.com/public_html/chimp && git pull');
-
-// Printing additional info
-echo '
-Last line of the output: ' . $gitpull . '
-?>
